2b66227db6c12ccee165e0a0e6baea2de3586f30
thierry <thierryars2@hotmail.com>
2023-09-08 16:08:52 +0200

    eq check for matrices

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,17 +1,19 @@
 use std::ops::{Index, IndexMut};
+use std::cmp::Eq;
+
 
 #[macro_use]
-// Looks like its better to create matrices from native types
-// Can use nalgebra, but this will prob make it slower
-// Should also look at canvas implementation
-// Check if I want to use matrices this way
 
+// Questions:
 // Functions need to be public to use them in ch03, but is this the only reason
-
 // SHould I use i32?
 // Any other way to do this?
 // Seems pretty clean, but maybe i got more options
 // Easy way to create, n*m 0 matrix maybe?
+// Implement Eq for comparing matrices -> what is the difference: ops, cmp
+
+
+
 #[derive(Debug, Clone)]
 pub struct Matrix {
     pub width: usize,
@@ -19,6 +21,7 @@ pub struct Matrix {
     pub data: Vec<Vec<i32>>,
     }
 
+// Looks like its better to create matrices from native types
 impl Matrix {
 
     pub fn new(width: usize, height: usize, data: Vec<Vec<i32>>) -> Self {
@@ -50,6 +53,15 @@ impl IndexMut<usize> for Matrix {
         &mut self.data[index]
     }
 }
+
+impl Eq for Matrix {
+
+    fn Eq(self, Eq: bool) -> 
+
+
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -84,9 +96,40 @@ mod tests {
         assert!(new_matrix[0][0] == -3);
         assert!(new_matrix[1][1] == -2);
         assert!(new_matrix[2][2] == 1);
+    }
+
+
+    // Should make it work 100%, does this mean with floating points aswell?
+    fn compare_matrices(){
+
+        let matrix_values = vec![
+            vec![-3, 5, 0],
+            vec![1, -2, -7],
+            vec![0, 1, 1],
+        ];
+    
+        let matrix_values_2 = vec![
+            vec![-1, 5, 0],
+            vec![1, -2, -7],
+            vec![0, 1, 1],
+        ];
+
+        let matrix_one = Matrix::new(3, 3, matrix_values);
+        let matrix_two = Matrix::new(3, 3, matrix_values_2);
+        let matrix_three = Matrix::new(3, 3, matrix_values);
+
+        assert!(matrix_one != matrix_one);
+        assert!((matrix_one == matrix_three));
 
     }
 
+
+    // should look at how i implemented multiplication for canvas
+    fn multiply_matrices(){
+
+
+
+    }
 
 
 }

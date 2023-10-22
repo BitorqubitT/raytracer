616bac6420f5cc4d8f196c228f6df03be431615e
thierry <thierryars2@hotmail.com>
2023-10-22 17:02:36 +0200

    changed matrix types to f64

--- src/matrix.rs
+++ src/matrix.rs
@@ -9,23 +9,27 @@ use crate::tuple::Tuple;
 // Implementing matrix comparison can be done by using partialEq -> how does this really work?
 
 // TODO:
-// Implement M * tuple
 // Use my own struct. How to create impl for m * tuple
-// INT vs FLT, how 
 // Matrix mul speed?
+
+// Change:
+// make everything float
+// M * Tuple = tuple  (how do i deal with different sizes?)
+
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Matrix {
     pub width: usize,
     pub height: usize,
-    pub data: Vec<Vec<i32>>,
+    pub data: Vec<Vec<f64>>,
     }
 
 // Looks like its better to create matrices from native types
 impl Matrix {
 
     // f64 instead of i32???
-    pub fn new(width: usize, height: usize, data: Vec<Vec<i32>>) -> Self {
+    pub fn new(width: usize, height: usize, data: Vec<Vec<f64>>) -> Self {
         Self {
             width,
             height,
@@ -42,15 +46,15 @@ impl Matrix {
 }
 
 impl Index<usize> for Matrix {
-    type Output = Vec<i32>;
+    type Output = Vec<f64>;
 
-    fn index(&self, index: usize) -> &Vec<i32> {
+    fn index(&self, index: usize) -> &Vec<f64> {
         &self.data[index]
     }
 }
 
 impl IndexMut<usize> for Matrix {
-    fn index_mut(&mut self, index: usize) -> &mut Vec<i32> {
+    fn index_mut(&mut self, index: usize) -> &mut Vec<f64> {
         &mut self.data[index]
     }
 }
@@ -65,14 +69,14 @@ impl Mul for Matrix {
         }
         
         // For now we create a matrix to make the impl work.
-        let matrix_values: Vec<Vec<i32>> = vec![vec![0; other.width]; self.height];
+        let matrix_values: Vec<Vec<f64>> = vec![vec![0.0; other.width]; self.height];
         let mut result = Matrix::new(other.width, self.height, matrix_values);
 
         for i in 0..self.height {
             for j in 0..other.width {
                 
                 // Type is i32 for now, don't we need flt64 for matrix?
-                let mut sum= 0;
+                let mut sum= 0.0;
                 for k in 0..self.width {
                     sum += self.data[i][k] * other.data[k][j];
                 }
@@ -83,18 +87,16 @@ impl Mul for Matrix {
        return result
     }
 
-    // No idea what to do here.
-    // Can use tuple or just 1d matrix.
-    // What is the play here.
-    // Can we mul m by tuple
-    // Should I use the tuple struct?
     // whats a logical implementation?
-   
-   // fn mul(self, other: Tuple) -> Self {
+    fn mul(self, other: Tuple) -> other {
+            
 
 
 
-    //}
+
+
+
+    }
 
 
 
@@ -108,33 +110,33 @@ mod tests {
     #[test]
     fn test_2d_matrix(){
         let matrix_values = vec![
-            vec![-3, 5],
-            vec![1, -2],
+            vec![-3.0, 5.0],
+            vec![1.0, -2.0],
         ];
 
         let new_matrix = Matrix::new(2, 2, matrix_values);
 
-        assert!(new_matrix[0][0] == -3);
-        assert!(new_matrix[0][1] == 5);
-        assert!(new_matrix[1][0] == 1);
-        assert!(new_matrix[1][1] == -2);
+        assert!(new_matrix[0][0] == -3.0);
+        assert!(new_matrix[0][1] == 5.0);
+        assert!(new_matrix[1][0] == 1.0);
+        assert!(new_matrix[1][1] == -2.0);
     }
 
     #[test]
     fn test_3d_matrix(){
         
         let matrix_values = vec![
-            vec![-3, 5, 0],
-            vec![1, -2, -7],
-            vec![0, 1, 1],
+            vec![-3.0, 5.0, 0.0],
+            vec![1.0, -2.0, -7.0],
+            vec![0.0, 1.0, 1.0],
         ];
 
         let new_matrix = Matrix::new(3, 3, matrix_values);
 
-        assert!(new_matrix[0][0] == -3);
-        assert!(new_matrix[1][1] == -2);
-        assert!(new_matrix[2][2] == 1);
-        assert!(new_matrix[0][1] == 5);
+        assert!(new_matrix[0][0] == -3.0);
+        assert!(new_matrix[1][1] == -2.0);
+        assert!(new_matrix[2][2] == 1.0);
+        assert!(new_matrix[0][1] == 5.0);
     }
 
     #[test]
@@ -142,15 +144,15 @@ mod tests {
     fn compare_matrices(){
 
         let matrix_values = vec![
-            vec![-3, 5, 0],
-            vec![1, -2, -7],
-            vec![0, 1, 1],
+            vec![-3.0, 5.0, 0.0],
+            vec![1.0, -2.0, -7.0],
+            vec![0.0, 1.0, 1.0],
         ];
     
         let matrix_values_2 = vec![
-            vec![-1, 5, 0],
-            vec![1, -2, -7],
-            vec![0, 1, 1],
+            vec![-1.0, 5.0, 0.0],
+            vec![1.0, -2.0, -7.0],
+            vec![0.0, 1.0, 1.0],
         ];
 
         let matrix_one = Matrix::new(3, 3, matrix_values);
@@ -163,18 +165,18 @@ mod tests {
     fn multiply_matrices_2by2(){
 
         let matrix_values_a = vec![
-            vec![2, 5],
-            vec![9, 51],
+            vec![2.0, 5.0],
+            vec![9.0, 51.0],
         ];
 
         let matrix_values_b = vec![
-            vec![6, 3],
-            vec![1, 81],
+            vec![6.0, 3.0],
+            vec![1.0, 81.0],
         ];
 
         let matrix_values_c = vec![
-            vec![17, 411],
-            vec![105, 4158],
+            vec![17.0, 411.0],
+            vec![105.0, 4158.0],
         ];
 
         let matrix_a = Matrix::new(2, 2, matrix_values_a);
@@ -188,20 +190,20 @@ mod tests {
     fn multiply_matrices_3_2_by_2_3(){
 
         let matrix_values_a = vec![
-            vec![2, 0],
-            vec![1, 11],
-            vec![955, 51],
+            vec![2.0, 0.0],
+            vec![1.0, 11.0],
+            vec![955.0, 51.0],
         ];
 
         let matrix_values_b = vec![
-            vec![6, 3, 1],
-            vec![1, 81, 9],
+            vec![6.0, 3.0, 1.0],
+            vec![1.0, 81.0, 9.0],
         ];
 
         let matrix_values_c = vec![
-            vec![12, 6, 2],
-            vec![17, 894, 100],
-            vec![5781, 6996, 1414],
+            vec![12.0, 6.0, 2.0],
+            vec![17.0, 894.0, 100.0],
+            vec![5781.0, 6996.0, 1414.0],
         ];
 
         // check what is the correct input
@@ -218,24 +220,24 @@ mod tests {
     fn multiply_matrices_4by4(){
 
         let matrix_values_a = vec![
-            vec![1, 2, 3, 4],
-            vec![5, 6, 7, 8],
-            vec![9, 8, 7, 6],
-            vec![5, 4, 3, 2]
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![5.0, 6.0, 7.0, 8.0],
+            vec![9.0, 8.0, 7.0, 6.0],
+            vec![5.0, 4.0, 3.0, 2.0]
         ];
 
         let matrix_values_b = vec![
-            vec![-2, 1, 2, 3],
-            vec![3, 2, 1, -1],
-            vec![4, 3, 6, 5],
-            vec![1, 2, 7, 8]
+            vec![-2.0, 1.0, 2.0, 3.0],
+            vec![3.0, 2.0, 1.0, -1.0],
+            vec![4.0, 3.0, 6.0, 5.0],
+            vec![1.0, 2.0, 7.0, 8.0]
         ];
 
         let matrix_values_c = vec![
-            vec![20, 22, 50, 48],
-            vec![44, 54, 114, 108],
-            vec![40, 58, 110, 102],
-            vec![16, 26, 46, 42]
+            vec![20.0, 22.0, 50.0, 48.0],
+            vec![44.0, 54.0, 114.0, 108.0],
+            vec![40.0, 58.0, 110.0, 102.0],
+            vec![16.0, 26.0, 46.0, 42.0]
         ];
 
         let matrix_a = Matrix::new(4, 4, matrix_values_a);
@@ -249,30 +251,30 @@ mod tests {
     fn multiply_matrices_6by6(){
 
         let matrix_values_a = vec![
-            vec![1, 2, 3, 4, 9, 1],
-            vec![5, 6, 7, 8, 1, 2],
-            vec![9, 8, 7, 6, 3, 5],
-            vec![5, 4, 3, 2, 6, 7],
-            vec![1, 82, 7, 1, 3, 5],
-            vec![9, 8, 7, 5, 3, 5],
+            vec![1.0, 2.0, 3.0, 4.0, 9.0, 1.0],
+            vec![5.0, 6.0, 7.0, 8.0, 1.0, 2.0],
+            vec![9.0, 8.0, 7.0, 6.0, 3.0, 5.0],
+            vec![5.0, 4.0, 3.0, 2.0, 6.0, 7.0],
+            vec![1.0, 82.0, 7.0, 1.0, 3.0, 5.0],
+            vec![9.0, 8.0, 7.0, 5.0, 3.0, 5.0],
         ];
 
         let matrix_values_b = vec![
-            vec![-2, 1, 2, 3, 9, 1],
-            vec![3, 2, 1, -1, 44, 2],
-            vec![4, 3, 6, 5, 15, 1],
-            vec![1, 2, 1, 8, 1, 4],
-            vec![1, 1, 6, 82, 45, 4],
-            vec![13, 6, 7, 1, 5, 4],
+            vec![-2.0, 1.0, 2.0, 3.0, 9.0, 1.0],
+            vec![3.0, 2.0, 1.0, -1.0, 44.0, 2.0],
+            vec![4.0, 3.0, 6.0, 5.0, 15.0, 1.0],
+            vec![1.0, 2.0, 1.0, 8.0, 1.0, 4.0],
+            vec![1.0, 1.0, 6.0, 82.0, 45.0, 4.0],
+            vec![13.0, 6.0, 7.0, 1.0, 5.0, 4.0],
             ];
 
         let matrix_values_c = vec![
-            vec![42, 37, 87, 787, 556, 64],
-            vec![71, 67, 86, 192, 477, 68],
-            vec![108, 91, 127, 353, 704, 88],
-            vec![113, 74, 119, 541, 573, 76],
-            vec![341, 221, 180, 215, 3883, 208],
-            vec![107, 89, 126, 345, 703, 84],
+            vec![42.0, 37.0, 87.0, 787.0, 556.0, 64.0],
+            vec![71.0, 67.0, 86.0, 192.0, 477.0, 68.0],
+            vec![108.0, 91.0, 127.0, 353.0, 704.0, 88.0],
+            vec![113.0, 74.0, 119.0, 541.0, 573.0, 76.0],
+            vec![341.0, 221.0, 180.0, 215.0, 3883.0, 208.0],
+            vec![107.0, 89.0, 126.0, 345.0, 703.0, 84.0],
         ];
 
 
@@ -288,10 +290,10 @@ mod tests {
     fn multiply_matrix_by_tuple(){
 
         let matrix_values_a = vec![
-            vec![1, 2, 3, 4],
-            vec![2, 4, 4, 2],
-            vec![8, 6, 4, 1],
-            vec![0, 0, 0, 1]
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![2.0, 4.0, 4.0, 2.0],
+            vec![8.0, 6.0, 4.0, 1.0],
+            vec![0.0, 0.0, 0.0, 1.0],
         ];
 
         let mut tuple_a = Tuple::new(2.0, 3.0, -1.0, 6.0);

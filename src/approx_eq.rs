d941923df97a2a4fef721810e0202813e13b2bbf
BitorqubitT <thierryars2@hotmail.com>
2025-02-03 23:33:33 +0100

    added test eq for tuple

--- src/approx_eq.rs
+++ src/approx_eq.rs
@@ -1,14 +1,32 @@
 use std::f64;
 
+use crate::tuple::Tuple;
+
 // Use this to check if certain numbers are equal up to x decimal points.
-pub trait ApproxEq {
-    fn fuzzy_eq(&self, other: f64) -> bool;
+// <T> defines a generic type
+pub trait ApproxEq<T> {
+    fn fuzzy_eq(&self, other: T) -> bool;
 }
 
-impl ApproxEq for f64 {
+impl ApproxEq<f64> for f64 {
     fn fuzzy_eq(&self, other: f64) -> bool{
         let epsilon = 1e-1;
         (*self - other).abs() < epsilon
+    }
+}
+
+// Use & to pass a reference, aka a point to the value in stead of the value
+// Then I need to also pass the reference in the tests
+// impl ApproxEq<&Tuple> for Tuple{
+// fn fuzzy_eq(&self, other: &Tuple) -> bool {
+
+impl ApproxEq<Tuple> for Tuple{
+    fn fuzzy_eq(&self, other: Tuple) -> bool {
+        let epsilon = 1e-1;
+        (self.x - other.x).abs() < epsilon &&
+        (self.y - other.y).abs() < epsilon &&
+        (self.z - other.z).abs() < epsilon
+        
     }
 }
 

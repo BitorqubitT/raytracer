534c935e853181e9911a2f7b4838fcb356d003ff
BitorqubitT <thierryars2@hotmail.com>
2025-02-10 22:01:29 +0100

    start impl sphere struct

---
+++ src/sphere.rs
@@ -0,0 +1,25 @@
+
+
+
+pub struct sphere(){
+ // somethign
+
+}
+
+impl sphere{
+
+   pub fn new() -> Self {
+        Self {
+        }
+    }
+
+
+    // Might need to move this function to other struct
+    pub fn intersect(){
+
+
+    }
+
+
+
+}

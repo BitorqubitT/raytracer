bd14910aff295ba547bebbe0e51e2099283696e5
BitorqubitT <thierryars2@hotmail.com>
2025-02-11 21:21:20 +0100

    test for ray intersect sphere

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,12 +1,10 @@
 
-
-
-pub struct sphere(){
+pub struct Sphere(){
  // somethign
 
 }
 
-impl sphere{
+impl Sphere{
 
    pub fn new() -> Self {
         Self {
@@ -21,5 +19,33 @@ impl sphere{
     }
 
 
+
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    
+    use crate::tuple::Tuple;
+
+    use super::*;
+
+    #[test]
+    fn ray_intersect_sphere_at_tangent(){
+        let point_a = Tuple::point_a(0.0, 1.0, -5.0);
+        let vector_a = Tuple::vector(0.0, 0.0, 1.0);
+        let sphere_a = Sphere::new();
+        let ray_a = Ray::new(point_a, vector_a);
+        let xs = Sphere.intersect(ray_a);
+
+        assert(len(xs) == 2);
+        assert(xs[0] == -6.0);
+        assert(xs[1] == -4.0);
+
+    }
+
+ 
 
 }

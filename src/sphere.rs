dce83f6514a45841c96ca64e636a319b45da6339
BitorqubitT <thierryars2@hotmail.com>
2025-02-11 21:49:27 +0100

    update intersect

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,5 +1,10 @@
+use std::mem::Discriminant;
+use crate::ray::*;
+use crate::matrix::*;
+use crate::tuple::*;
+use std::ops::{Index, IndexMut, Mul, Sub, };
 
-pub struct Sphere(){
+pub struct Sphere{
  // somethign
 
 }
@@ -11,13 +16,31 @@ impl Sphere{
         }
     }
 
-
+    //TODO:
+    // I sometimes use let dot_computed: f64 = vec_one.dot(vec_two);
+    // BUt also dot(vecone, vectwo);
+    // Checkl this
     // Might need to move this function to other struct
-    pub fn intersect(){
+    pub fn intersect(ray: Ray) {
 
+        let sphere_to_ray = ray.origin - Tuple::point(0.0, 0.0, 0.0);
 
+        //let a = dot(ray.direction, ray.direction);
+        let a = ray.direction.dot(ray.direction);
+        //let b = 2 * dot(ray.direction, sphere_to_ray);
+        //let c = dot(sphere_to_ray, sphere_to_ray) - 1;
+        let b = 2.0 * ray.direction.dot(ray.direction);
+        let c = sphere_to_ray.dot(sphere_to_ray) - 1.0;
+        let discriminant = (b * b) - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return ()
+        } else {
+            let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
+            let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+            return (t1, t2);
+        }
     }
-
 
 
 }
@@ -34,13 +57,13 @@ mod tests {
 
     #[test]
     fn ray_intersect_sphere_at_tangent(){
-        let point_a = Tuple::point_a(0.0, 1.0, -5.0);
+        let point_a = Tuple::point(0.0, 1.0, -5.0);
         let vector_a = Tuple::vector(0.0, 0.0, 1.0);
         let sphere_a = Sphere::new();
         let ray_a = Ray::new(point_a, vector_a);
         let xs = Sphere.intersect(ray_a);
 
-        assert(len(xs) == 2);
+        assert(len(xs) == 2.0);
         assert(xs[0] == -6.0);
         assert(xs[1] == -4.0);
 

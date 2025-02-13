1666823e3221de4c8f5dacf80b997bad8e8d94cb
BitorqubitT <thierryars2@hotmail.com>
2025-02-13 22:19:28 +0100

    added other files

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 
 // Add traits here, so they can be found and used in the other files/the chapters
-
 pub mod tuple;
 pub mod canvas;
 pub mod matrix;
 pub mod approx_eq;
 pub mod ray;
 pub mod sphere;
+pub mod intersection;
